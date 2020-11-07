08fcaa23a44f6c136f74237d529b589d580261b3
Wen <changyaowen19980629@gmail.com>
2020-11-07 15:44:20 +0800

    20201107

--- GYM/aimidterm.py
+++ GYM/aimidterm.py
@@ -4,6 +4,7 @@ import matplotlib.animation as animation
 from numpy.random import seed
 from numpy.random import randint
 import pygame
+import time
 
 
 # 不限程式語言實作人工生命(Artificial life)，主題與方法不限。
@@ -24,12 +25,14 @@ import pygame
 
 # this function is for changing the exist dots
 
+# move -> update Person statement
+
 class Person:
     def __init__(self, x, y, figure):
         self.x = x
         self.y = y
         self.figure = figure
-        self.nextSecondChangeFigure = None
+
         self.white = (255, 255, 255)  # grid background
         self.black = (0, 0, 0)  # personal trainer
         self.pink = (255, 105, 180)  # fat woman
@@ -37,21 +40,22 @@ class Person:
         self.blue = (0, 0, 255)  # man
         self.red = (255, 0, 0)  # woman
         self.green = (0, 255, 0)  # personal trainer
-        if figure == 0:
-            self.color = self.white
-        elif figure == 1:
-            self.color = self.blue
-        elif figure == 2:
-            self.color = self.red
-        elif figure == 3:
-            self.color = self.green
-        elif figure == 4:
-            self.color = self.pink
-        elif figure == 5:
-            self.color = self.purple
 
-    def move(self):
-        pass
+        self.colorUpdate()
+
+    def colorUpdate(self):
+        if self.figure == 0:
+            self.color = self.white
+        elif self.figure == 1:
+            self.color = self.blue
+        elif self.figure == 2:
+            self.color = self.red
+        elif self.figure == 3:
+            self.color = self.green
+        elif self.figure == 4:
+            self.color = self.pink
+        elif self.figure == 5:
+            self.color = self.purple
 
 
 class Gym:
@@ -66,97 +70,119 @@ class Gym:
         self.white = (255, 255, 255)  # grid background
         self.black = (0, 0, 0)  # personal trainer
         '''initialize gym layout'''
-        self.manRatio = 0.2
-        self.womanRatio = 0.2
+        self.manRatio = 0.1
+        self.womanRatio = 0.1
         self.fatManRatio = 0.025
         self.fatWomanRatio = 0.025
         self.trainerRatio = 0.05
-        self.gymSpaceRatio = 0.5
+        self.gymSpaceRatio = 0.7
         self.screen = pygame.display.set_mode((windowHeight, windowWidth))
         self.screen.fill(self.white)
-        self.drawGrid()
         self.generateInitRandom()
 
-    def update(self):
-        pass
+    def move(self):  # mve obj to random empty space
+        for i in range(self.gridHeight):
+            self.debug()
+            for j in range(self.gridWidth):
+                if self.layout[i, j].figure != 0:
+                    nextStep = self.checkNextStep(i, j)
+
+                    if nextStep != 0:
+                        # print(nextStep[0], nextStep[1])
+                        # print("before: ", self.layout[nextStep[0], nextStep[1]].figure)
+                        self.layout[nextStep[0], nextStep[1]] = self.layout[i][j]
+                        # print("after: ", self.layout[nextStep[0], nextStep[1]].figure)
+                        self.layout[i, j].figure = 0
+
+        exit()
+
+    def checkNextStep(self, i, j):
+        if i - 1 >= 0 and self.layout[i - 1, j].figure == 0:
+            return (i - 1, j)
+        elif j - 1 >= 0 and self.layout[i, j - 1].figure == 0:
+            return (i, j - 1)
+        elif j + 1 < self.gridWidth and self.layout[i, j + 1].figure == 0:
+            return (i, j + 1)
+        elif i + 1 < self.gridHeight and self.layout[i + 1, j].figure == 0:
+            return (i + 1, j)
+        else:
+            return 0
+
+    def checkSurrounding(self, i, j,
+                         target):  # check surrounding objects has infect on current object, target is a tuple
+        for _i in range(-1, 2, 1):
+            for _j in range(-1, 2, 1):
+                if (0 <= i + _i < self.gridHeight) and (0 <= j + _j < self.gridWidth) and self.layout[
+                    i + _i, j + _j].figure in target:
+                    return (i + _i, j + _j)
+        return 0
+
+    def updateLayout(self):
+        for i in range(self.gridHeight):
+            for j in range(self.gridWidth):
+                rect = pygame.Rect(j * self.blockSize, i * self.blockSize, self.blockSize, self.blockSize)
+                # print(i, j, self.layout[i, j].figure)
+                pygame.draw.rect(self.screen, self.layout[i, j].color, rect)
+
+    def updateObjCondition(self):
+        for i in range(self.gridHeight):
+            for j in range(self.gridWidth):
+                if self.layout[i, j].figure == 0:  # empty
+                    continue
+                elif self.layout[i, j].figure == 1:  # man
+                    pass
+                    targetObjCoor = self.checkSurrounding(i, j, (2, -1))
+                    if targetObjCoor != 0:
+                        self.layout[targetObjCoor[0], targetObjCoor[1]].figure = 5
+                    # Person(0) problem: location to find empty space put new create obj
+                elif self.layout[i, j].figure == 2:  # woman
+                    pass
+                    targetObjCoor = self.checkSurrounding(i, j, (1, -1))
+                    if targetObjCoor != 0:
+                        self.layout[targetObjCoor[0], targetObjCoor[1]].figure = 4
+                    # problem: location to find empty space put new create obj
+                elif self.layout[i, j].figure == 3:  # coach
+                    targetObjCoor = self.checkSurrounding(i, j, (4, 5))
+                    if targetObjCoor != 0:
+                        if self.layout[targetObjCoor[0], targetObjCoor[1]].figure == 4:
+                            self.layout[targetObjCoor[0], targetObjCoor[1]].figure = 2
+                        else:
+                            self.layout[targetObjCoor[0], targetObjCoor[1]].figure = 1
+                elif self.layout[i, j].figure == 4:  # fat girl
+                    targetObjCoor = self.checkSurrounding(i, j, (3, -1))
+                    if targetObjCoor != 0:
+                        self.layout[i, j].figure = 2
+
+                elif self.layout[i, j].figure == 5:  # fat boy
+                    targetObjCoor = self.checkSurrounding(i, j, (3, -1))
+                    if targetObjCoor != 0:
+                        self.layout[i, j].figure = 1
+                self.layout[i, j].colorUpdate()
 
     def generateInitRandom(self):
-        objQuantity = int(pow(self.windowHeight / self.blockSize, 2))
+        self.gridHeight = int(self.windowHeight / self.blockSize)
+        self.gridWidth = int(self.windowWidth / self.blockSize)
+        objQuantity = int(pow(self.gridWidth, 2))
 
         self.objIndiceList = [0, 1, 2, 3, 4,
                               5]  # val (0, 1, 2, 3, 4, 5) means (gymSpace, man, woman, coach, fat_girl, fat_boy)
         self.objLocList = np.random.choice(self.objIndiceList, objQuantity,
                                            p=[self.gymSpaceRatio, self.manRatio, self.womanRatio, self.trainerRatio,
                                               self.fatWomanRatio, self.fatManRatio])
-        self.objLocList = self.objLocList.reshape(
-            (int(self.windowHeight / self.blockSize), int(self.windowWidth / self.blockSize)))
+        self.objLocList = self.objLocList.reshape((self.gridHeight, self.gridWidth))
+        self.layout = np.empty([100, 100], dtype=object)
 
-        for i in range(int(self.windowHeight / self.blockSize)):
-            for j in range(int(self.windowWidth / self.blockSize)):
-                self.objLocList[i, j] = Person(i, j, self.objLocList[i, j])
+        for i in range(self.gridHeight):
+            for j in range(self.gridWidth):
+                self.layout[i, j] = Person(i, j, self.objLocList[i, j])
 
-    def nextMoveCheck(self):
-        pass
+    def debug(self):
+        arr = np.zeros((self.gridHeight, self.gridWidth), int)
+        for i in range(self.gridHeight):
+            for j in range(self.gridWidth):
+                arr[i, j] = self.layout[i, j].figure
 
-    def drawGrid(self):
-        self.blockSize = 20  # Set the size of the grid block
-        for i in range(self.windowHeight):
-            for j in range(self.windowWidth):
-                rect = pygame.Rect(j * self.blockSize, i * self.blockSize, self.blockSize,
-                                   self.blockSize)  # left, top, width, height
-                pygame.draw.rect(self.screen, self.black, rect, 1)
-
-
-def operationOfCurrentPoint(sumOfSurviver, i, j):
-    if (orgGrid[i, j] == 255):  # current point survive
-        if (sumOfSurviver <= 1):
-            tmpGrid[i, j] = 0
-        elif (sumOfSurviver <= 3):
-            pass
-        else:
-            tmpGrid[i, j] = 0
-    else:
-        if (sumOfSurviver == 3):
-            tmpGrid[i, j] = 255
-        else:
-            pass
-
-
-def checkSurrounding(i, j):  # checking sorrounding surviver
-    sumOfSurviver = 0
-
-    if (i - 1 >= 0 and j - 1 >= 0 and orgGrid[i - 1, j - 1] == 255):  # left-up
-        sumOfSurviver += 1
-    if (i - 1 >= 0 and orgGrid[i - 1, j] == 255):  # up
-        sumOfSurviver += 1
-    if (i - 1 >= 0 and j + 1 < gridSize and orgGrid[i - 1, j + 1] == 255):  # right-up
-        sumOfSurviver += 1
-    if (j - 1 >= 0 and orgGrid[i, j - 1] == 255):  # left
-        sumOfSurviver += 1
-    if (j + 1 < gridSize and orgGrid[i, j + 1] == 255):  # right
-        sumOfSurviver += 1
-    if (i + 1 < gridSize and j - 1 >= 0 and orgGrid[i + 1, j - 1] == 255):  # left-down
-        sumOfSurviver += 1
-    if (i + 1 < gridSize and orgGrid[i + 1, j] == 255):  # down
-        sumOfSurviver += 1
-    # right-down
-    if (i + 1 < gridSize and j + 1 < gridSize and orgGrid[i + 1, j + 1] == 255):
-        sumOfSurviver += 1
-
-    operationOfCurrentPoint(sumOfSurviver, i, j)
-
-
-def update(self):  # update the current mat
-    # sumOfSurviver = 0
-    global orgGrid, tmpGrid
-    tmpGrid = orgGrid.copy()
-    for i in range(gridSize):
-        for j in range(gridSize):
-            checkSurrounding(i, j)
-
-    mat.set_data(tmpGrid)
-    orgGrid = tmpGrid
-    return [mat]
+        print(arr)
 
 
 def main():
@@ -169,28 +195,26 @@ def main():
     clock = pygame.time.Clock()
     gym = Gym(windowHeight, windowWidth, blockSize)
 
+    gym.debug()
+
     while True:
-        gym.drawGrid()
+        # gym.move()
+        # gym.debug()
+        # return\
+        gym.updateObjCondition()
+
+        gym.updateLayout()
+
+        # gym.debug()
+
+        # return
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
+        # gym.debug()
         pygame.display.update()
-
-    '''
-    global mat, orgGrid, gridSize
-
-    gridSize = 300
-    vals = [255, 0]
-    orgGrid = np.random.choice(
-        vals, gridSize*gridSize, p=[0.2, 0.8]).reshape(gridSize, gridSize)
-
-    # below draw the figure
-    figure, axes = plt.subplots()
-    mat = axes.matshow(orgGrid)
-    ani = animation.FuncAnimation(figure, update, interval=50, save_count=50)
-    plt.show()
-    '''
+        time.sleep(1)
 
 
 if __name__ == "__main__":
